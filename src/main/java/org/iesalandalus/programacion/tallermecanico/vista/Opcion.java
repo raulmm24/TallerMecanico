520907a9729f3ab4a9427dc163221cc95743ea64
raulm <raulmartinezmartinez24@gmail.com>
2026-03-10 14:50:25 +0100

    Realización de la clase Opcion

---
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/vista/Opcion.java
@@ -0,0 +1,59 @@
+package org.iesalandalus.programacion.tallermecanico.vista;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public enum Opcion {
+
+    INSERTAR_CLIENTE(1,"Insertar cliente"),
+    BUSCAR_CLIENTE(2,"Buscar cliente"),
+    BORRAR_CLIENTE(3,"Borrar cliente"),
+    LISTAR_CLIENTES(4,"Listar clientes"),
+    MODIFICAR_CLIENTE(5,"Modificar cliente"),
+    INSERTAR_VEHICULO(6,"Insertar vehículo"),
+    BUSCAR_VEHICULO(7,"Buscar vehículo"),
+    BORRAR_VEHICULO(8,"Borrar vehículo"),
+    LISTAR_VEHICULOS(9,"Listar vehículos"),
+    INSERTAR_REVISION(10,"Insertar revisión"),
+    BUSCAR_REVISION(11,"Buscar revisión"),
+    BORRAR_REVISION(12,"Borrar revisión"),
+    LISTAR_REVISIONES(13,"Listar revisiones"),
+    LISTAR_REVISIONES_CLIENTE(14,"Listar revisiones un cliente"),
+    LISTAR_REVISIONES_VEHICULO(15,"Listar revisiones de un vehículo"),
+    ANADIR_HORAS_REVISION(16,"Añadir horas a una revisión"),
+    ANADIR_PRECIO_MATERIAL_REVISION(17,"Añadir precio de material a una revisión"),
+    CERRAR_REVISION(18,"Cerrar revisión"),
+    SALIR(0,"Salir");
+
+    private final int numeroOpcion;
+    private final String mensaje;
+
+    private static final Map<Integer,Opcion> opciones = new HashMap<>();
+
+    static {
+        for (Opcion opcion : values()) {
+            opciones.put(opcion.numeroOpcion, opcion);
+        }
+    }
+
+    private Opcion(int numeroOpcion,String mensaje) {
+        this.numeroOpcion = numeroOpcion;
+        this.mensaje = mensaje;
+    }
+
+    public static boolean esValida(int numeroOpcion) {
+        return opciones.containsKey(numeroOpcion);
+    }
+
+    public static Opcion get(int numeroOpcion) {
+        if (!esValida(numeroOpcion)) {
+            throw new IllegalArgumentException("ERROR: el número de opción no es correcto.");
+        }
+        return opciones.get(numeroOpcion);
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%d. %s", numeroOpcion, mensaje);
+    }
+}
