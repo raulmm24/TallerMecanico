d80e249bb72ddf0307af69b2089a1bbcfd026ef7
raulm <raulmartinezmartinez24@gmail.com>
2026-04-23 14:31:29 +0200

    Modificación del Main

--- src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
+++ src/main/java/org/iesalandalus/programacion/tallermecanico/Main.java
@@ -1,7 +1,25 @@
 package org.iesalandalus.programacion.tallermecanico;
 
+import org.iesalandalus.programacion.tallermecanico.controlador.Controlador;
+import org.iesalandalus.programacion.tallermecanico.controlador.IControlador;
+import org.iesalandalus.programacion.tallermecanico.modelo.FabricaModelo;
+import org.iesalandalus.programacion.tallermecanico.modelo.Modelo;
+import org.iesalandalus.programacion.tallermecanico.modelo.negocio.FabricaFuenteDatos;
+import org.iesalandalus.programacion.tallermecanico.vista.FabricaVista;
+import org.iesalandalus.programacion.tallermecanico.vista.Vista;
+
 public class Main {
     public static void main(String[] args) {
+        try {
+            Modelo modelo = FabricaModelo.CASCADA.crear(FabricaFuenteDatos.MEMORIA);
 
+            Vista vista = FabricaVista.TEXTO.crear();
+
+            IControlador controlador = new Controlador(modelo,vista);
+
+            controlador.comenzar();
+        } catch (Exception e) {
+            System.err.println("ERROR" + e.getMessage());
+        }
     }
 }
